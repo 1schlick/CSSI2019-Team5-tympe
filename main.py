69f9b9b14087b330a4e7acb2ff10790c9a054b00
1schlick <53189655+1schlick@users.noreply.github.com>
2019-08-05 15:08:47 -0700

    python hell

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 import webapp2
 import os
 import jinja2
+import random
+
+
 
 jinja_current_dir = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
-class thingy(webapp2.RequestHandler):
+
+
+class welcome(webapp2.RequestHandler):
     def get(self):
         start_template = jinja_current_dir.get_template("Welcome.html")
         self.response.write(start_template.render())
 
     def post(self):
+        f = open("words.txt", "r")
+        fl = []
+
+        for x in f:
+            fl.append(x)
+
+        randWord = fl[random.randint(0,999)]
+        dict = {"word": randWord}
         end_template = jinja_current_dir.get_template("Game.html")
-        self.response.write(end_template.render())
+        self.response.write(end_template.render(dict))
+
+class game(webapp2.RequestHandler):
+    def get (self):
+        
+
 
 app = webapp2.WSGIApplication([
-    ('/', thingy)
+    ('/', welcome),
+    ('/game', game)
 ], debug=True)
